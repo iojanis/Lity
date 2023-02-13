b363b394e55e70753e341c3768d93b53b14fe24a
Janis Jendraß <me@janis.io>
2023-02-13 18:53:21 +0100

    1.5.0 Release

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import { VitePWA } from "vite-plugin-pwa/dist";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import pkg from "./package.json";
@@ -35,40 +34,6 @@ export default defineConfig(async () => {
           refSugar: true,
         },
       }),
-      // VitePWA({
-      //   registerType: "autoUpdate",
-      //   strategies: "generateSW",
-      //   includeAssets: [
-      //     "favicon.svg",
-      //     "favicon.ico",
-      //     "robots.txt",
-      //     "apple-touch-icon.png",
-      //   ],
-      //   manifest: {
-      //     name: process.env.APP_LONG_TITLE,
-      //     short_name: process.env.APP_TITLE,
-      //     description: process.env.APP_DESCRIPTION,
-      //     theme_color: "#ffffff",
-      //     icons: [
-      //       {
-      //         src: "android-chrome-192x192.png",
-      //         sizes: "192x192",
-      //         type: "image/png",
-      //       },
-      //       {
-      //         src: "android-chrome-512x512.png",
-      //         sizes: "512x512",
-      //         type: "image/png",
-      //       },
-      //       {
-      //         src: "android-chrome-512x512.png",
-      //         sizes: "512x512",
-      //         type: "image/png",
-      //         purpose: "any maskable",
-      //       },
-      //     ],
-      //   },
-      // }),
       VitePluginHtmlEnv(),
     ],
         resolve: {
